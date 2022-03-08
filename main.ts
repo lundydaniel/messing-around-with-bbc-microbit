e7fbfab012cf495845b782b34d6dd0e4c8996a0c
Dan Lundy <72224080+lundydaniel@users.noreply.github.com>
2022-03-07 20:29:10 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+let baseline = input.magneticForce(Dimension.Strength)
 basic.forever(function () {
-	
+    if (Math.abs(input.magneticForce(Dimension.Strength)) - baseline > 100) {
+        basic.showIcon(IconNames.Giraffe)
+    } else {
+        basic.clearScreen()
+    }
 })
